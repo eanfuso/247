0f51e11e222ba86ce2d1e693d968ae8c66f8f631
esteban <esteban.anfuso@gmail.com>
2022-03-20 13:10:54 +0100

    conmutacion2

---
+++ src/main/java/com/tvp/pomi_02/Terminar.java
@@ -0,0 +1,40 @@
+package com.tvp.pomi_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Terminar {
+
+	
+	
+	
+	
+	
+	public Terminar() {
+		super();
+	}
+	
+	public void TerminarProceso () throws IOException {
+	String[] mat = {"taskkill", "/f", "/IM", "ffmpeg.exe"};
+	String[] pid = {"tasklist"};
+	ProcessBuilder pb = new ProcessBuilder().command(mat).redirectErrorStream(true);
+	Process pidb = pb.start();
+	
+	
+//	Runtime runtime =Runtime.getRuntime();
+//	Process process = runtime.exec(mat);
+	
+	
+	 
+//  InputStreamReader in = new InputStreamReader(pidb);
+//	Stream<String> sos = new BufferedReader(in).lines();
+//	String soso = sos.collect(Collectors.joining());         taskkill f /IM ffmpeg.exe
+	
+	
+	}
+	
+	
+}
